124496bc26093de7ed28730bab83a0fbfacd9275
pass2u.ssd <contact@micromacro.com.tw>
2017-04-20 11:20:26 +0800

    first commit - construct project structure

---
+++ mock-server/src/main/java/com/ajoshow/playground/MockServerApplication.java
@@ -0,0 +1,12 @@
+package com.ajoshow.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerApplication.class, args);
+	}
+}
